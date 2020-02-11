dad6b3e1c12e52b4f1b83f0d81075437d8cffbcf
mtobbias <marcelo.silva@mbamobi.com.br>
2020-02-11 19:29:41 -0300

    release

--- example/README.md
+++ example/README.md
@@ -1,16 +1,15 @@
-# validadores_example
+# Validador (PT-BR)
 
-Demonstrates how to use the validadores plugin.
 
-## Getting Started
+Este projeto tem como objetivo principal aplicação de estudos Flutter, e colaborar com a comunidade.
 
-This project is a starting point for a Flutter application.
+#Exemplo de utilização.
 
-A few resources to get you started if this is your first Flutter project:
 
-- [Lab: Write your first Flutter app](https://flutter.dev/docs/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://flutter.dev/docs/cookbook)
-
-For help getting started with Flutter, view our
-[online documentation](https://flutter.dev/docs), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+Validador()
+    .add(Validar.CPF, msg: 'CPF Inválido')
+    .add(Validar.OBRIGATORIO, msg: 'Campo obrigatório')
+    .minLength(11)
+    .maxLength(11)
+    .valido(value,clearNoNumber: true);
+    
---
+++ example/lib/main.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:validadores/Validador.dart';
+
+void main() => runApp(MalidadoresMain());
+
+class MalidadoresMain extends StatelessWidget {
+
+
+  @override
+  Widget build(BuildContext context) {
+
+    final _formKey = GlobalKey<FormState>();
+
+    return MaterialApp(
+      title: 'Validador',
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Validadores'),
+        ),
+        body: Form(
+          key: _formKey,
+          child: Container(
+            padding: EdgeInsets.all(20),
+            child: Column(
+              children: <Widget>[
+                TextFormField(
+                  validator: (value) {
+                    // Aqui entram as validações
+
+                    return Validador()
+                        .add(Validar.CPF, msg: 'CPF Inválido')
+                        .add(Validar.OBRIGATORIO, msg: 'Campo obrigatório')
+                        .minLength(11)
+                        .maxLength(11)
+                        .valido(value,clearNoNumber: true);
+
+                  },
+                  decoration: InputDecoration(
+                    helperText: 'informe seu cpf',
+                    hintText: '123.456.789-00',
+                    labelText: 'CPF'
+                  ),
+                ),
+        RaisedButton(
+          onPressed: () {
+            if (_formKey.currentState.validate()) {
+              print('Valido');
+            }
+          },
+          child: Text('Enviar'),
+        )
+              ],
+            )
+          ),
+        ),
+      )
+      ,
+    );
+  }
+
+}
